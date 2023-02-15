d89305507f4d48caf7ebdc4cc5c5cc659d55ddf2
Bruno Bruno <b.bruno@1xinternet.de>
2023-02-15 19:42:27 +0100

    #1: initial commit

---
+++ script.js
@@ -0,0 +1,131 @@
+require('dotenv').config();
+
+// Get the hash of the url
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+window.location.hash = "";
+
+// Set token
+let _token = hash.access_token;
+
+const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const clientId = process.env.CLIENT_ID;
+// const redirectUri = "https://brunommbruno.github.io/Rhythmi/";
+const redirectUri = "http://localhost:5500/";
+const scopes = [
+  "streaming",
+  "user-modify-playback-state",
+  "user-library-modify"
+];
+
+// If there is no token, redirect to Spotify authorization
+if (!_token) {
+  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}&response_type=token`;
+}
+
+// Set up the Web Playback SDK
+
+let deviceId;
+let ids = [];
+
+window.onSpotifyPlayerAPIReady = () => {
+  const player = new Spotify.Player({
+    name: "Rhythmi",
+    getOAuthToken: cb => {
+      cb(_token);
+    }
+  });
+
+  // Error handling
+  player.on("initialization_error", e => console.error(e));
+  player.on("authentication_error", e => console.error(e));
+  player.on("account_error", e => console.error(e));
+  player.on("playback_error", e => console.error(e));
+
+  // Playback status updates
+  player.on("player_state_changed", state => {
+    console.log(state);
+  });
+
+  // Ready
+  player.on("ready", data => {
+    console.log("Ready with Device ID", data.device_id);
+    deviceId = data.device_id;
+  });
+
+  // Connect to the player!
+  player.connect();
+};
+
+// Play a specified track on the Web Playback SDK's device ID
+function play(device_id, track) {
+  $.ajax({
+    url: "https://api.spotify.com/v1/me/player/play?device_id=" + device_id,
+    type: "PUT",
+    data: `{"uris": ["${track}"]}`,
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "Bearer " + _token);
+    },
+    success: function(data) {
+      console.log(data);
+    }
+  });
+}
+
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function getASong() {
+  let random_seed = makeid(2);
+  let random_offset = Math.floor(Math.random() * 2000); // returns a random integer from 0 to 9
+  $.ajax({
+    url:
+      "https://api.spotify.com/v1/search?type=track&offset=" +
+      random_offset +
+      "&limit=1&q=" +
+      'genre:rock',
+    type: "GET",
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "Bearer " + _token);
+    },
+    success: function(data) {
+      console.log(data);
+      let trackUri = data.tracks.items[0].uri;
+
+      // play(deviceId, trackUri);
+      $("#current-track-name-save").attr("data-song", data.tracks.items[0].uri);
+      $("#current-track-name-save").attr(
+        "src",
+        "https://cdn.glitch.com/eed3cfeb-d097-4769-9d03-2d3a6cc7c004%2Ficons8-heart-24.png?v=1597232027543"
+      );
+      $("#embed-uri").attr(
+        "src",
+        "https://open.spotify.com/embed/track/" + data.tracks.items[0].id
+      );
+      $("#current-track-name-save").css("display", "block");
+    },
+    error: function() {
+      // run the function again in case of an error response
+      getASong();
+    }
+  });
+}
